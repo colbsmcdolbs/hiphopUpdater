5261d1a51650b2638cebe68045d42d311b394d0a
Colby Allen <colbyjayallen@gmail.com>
2019-07-30 22:25:12 -0600

    TODO: Ixnay on the Python bootstrap, follow the tutorial for better styling.

--- app/__init__.py
+++ app/__init__.py
@@ -3,14 +3,16 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_mail import Mail
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 mail = Mail(app)
+bootstrap = Bootstrap(app)
 
-from app import routes, models
+from app import routes, models, errors
 from app.scraper import scrape
 from app.models import clear_posts
 import atexit
