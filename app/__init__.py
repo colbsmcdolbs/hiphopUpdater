06b2b61f4c388fd3c1b21ae2e3b276188f68166f
Colby Allen <colbyjayallen@gmail.com>
2019-08-18 21:26:50 -0600

    Added BP

--- app/__init__.py
+++ app/__init__.py
@@ -24,6 +24,14 @@ def create_app(config_class=Config):
     mail.init_app(app)
     bootstrap.init_app(app)
 
+    from app.errors import bp as errors_bp
+    app.register_blueprint(errors_bp)
+
+    from app.main import bp as main_bp
+    app.register_blueprint(main_bp)
+
+
+
     if not app.debug and not app.testing:
         from app.scraper import scrape
         from app.models import clear_posts
--- app/main/routes.py
+++ app/main/routes.py
@@ -1,16 +1,17 @@
 from flask import render_template, flash, redirect, url_for
-from app import app
-from app.forms import UnsubForm,SignUpForm
-from app.models import User, Rapper, delete_user, import_user
+from app import db
+from app.forms import UnsubForm, SignUpForm
+from app.models import User, delete_user, import_user
+from app.main import bp
 
 
-@app.route('/')
-@app.route('/index')
+@bp.route('/')
+@bp.route('/index')
 def index():
     return render_template('index.html', title='Home')
 
 
-@app.route('/signup', methods=['GET', 'POST'])
+@bp.route('/signup', methods=['GET', 'POST'])
 def signup():
     form = SignUpForm()
     if form.validate_on_submit():
@@ -22,7 +23,7 @@ def signup():
     return render_template('signup.html', title='Sign Up', form=form)
 
 
-@app.route('/unsub', methods=['GET', 'POST'])
+@bp.route('/unsub', methods=['GET', 'POST'])
 def unsub():
     form = UnsubForm()
     if form.validate_on_submit():
@@ -35,6 +36,6 @@ def unsub():
     return render_template('unsub.html', title='Unsubscribe', form=form)
 
 
-@app.route('/success')
+@bp.route('/success')
 def unsubsuccess():
     return render_template('success.html', title='Success!')
